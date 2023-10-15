3afa36a6344afef65dae8c7e9c3ae971c5230954
11a <11a_git@proton.me>
2023-10-15 17:15:26 +0200

    routed home page correctly

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Skull from '../components/Skull.vue'
 import AppAccounts from '../components/AppAccounts.vue'
+import Landing from '../components/Landing_Page.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/accounts',
     name: 'AppAccounts',
     component: AppAccounts
+  },
+  {
+    path: '/'
+    , name: 'home'
+    , component: Landing
   }
 ]
 
