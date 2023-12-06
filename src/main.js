262b160c1ef38523b9478d4e8deec4428903297f
sanabarakat <sbarakat.ieu2021@student.ie.edu>
2023-12-06 16:45:48 +0100

    insightsfe

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import BootstrapVue from "bootstrap-vue";
 
 Vue.config.productionTip = false
 
+appInsights.setup({
+  instrumentationKey: 'c8b1a1f2-8c2c-4f2a-9d4e-2a4b7d1f9f4d',
+  connectionString: 'InstrumentationKey=6aec6ece-5fd6-40fb-a965-01f9e88c220e;IngestionEndpoint=https://northcentralus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northcentralus.livediagnostics.monitor.azure.com/'
+}).start();
+
+
 Vue.use(BootstrapVue);
 
 new Vue({
