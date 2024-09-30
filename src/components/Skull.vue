476b6353718e27e41a97bf1d3deedfef6d95cf6a
Alvaro Guadamillas <alguadam@microsoft.com>
2024-09-30 19:21:51 +0200

    Update development environment variables

--- src/components/Skull.vue
+++ src/components/Skull.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
         <h1>Skull</h1>
+        <p>Environment variables read from file {{ env_var_file_name }}</p>
         <p>{{ msg }}</p>
+        <p>Environment variables have been set in file: {{ env_var_file_name }}</p>
+        <p>It it is running on the environment: {{ environment }}</p>
+        <p>Backend URL is: {{ backend_url }}</p>
         <button type="button" class="btn btn-secondary">
-            Skull button
+            This is an example Skull button
         </button>
 
     </div>
 </template>
- 
+
 <script>
 import axios from 'axios'
 export default
@@ -16,7 +20,10 @@ export default
         name: 'Skull',
         data() {
             return {
-                msg: `Hi! This is the skull component 💀, running in mode: ${process.env.NODE_ENV} with environment variables defined in ${process.env.VUE_APP_ENV_VAR_FILE_NAME}`
+                msg: 'Hi! This is the skull component 💀',
+                environment: process.env.NODE_ENV,
+                env_var_file_name: process.env.VUE_APP_ENV_VAR_FILE_NAME,
+                backend_url: process.env.VUE_APP_ROOT_URL
             }
         },
         methods:
@@ -26,8 +33,6 @@ export default
                 axios.get(path)
                     .then((response) => {
                         this.msg = response.data;
-                        this.environment = process.env.NODE_ENV;
-                        this.env_var_file_name = process.env.VUE_APP_ENV_VAR_FILE_NAME
                     })
                     .catch(error => console.log(error))
             }
@@ -35,7 +40,5 @@ export default
         created() {
             this.getSkull()
         }
-
-
     }
 </script>
